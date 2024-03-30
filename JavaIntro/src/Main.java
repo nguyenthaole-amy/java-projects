7848c22b2bf2d7eddd243da4016e701212d34d8c
Amy Le <le46t@mtholyoke.edu>
2024-03-29 20:37:07 -0400

    update 0329

---
+++ JavaIntro/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    /**
+     * How Java works:
+     * Each file in Java is a class and they have to have the same name with the class you want to implement.
+     * In order to run a class, you have to have a "main" method that takes in the .
+     * */
+    //method (function) declare here
+    //method has to be declared outside of main function
+    /**
+     * this method takes in an integer and print it out
+     * @param a
+     */
+    public static void printInt(int a) {
+
+    }
+
+    public static void main(String[] args) {
+        //variable declaration
+        int a = new Integer(1);
+        //print
+        System.out.println("Hello world!");
+    }
+}
