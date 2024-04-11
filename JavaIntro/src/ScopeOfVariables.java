a1e6a2a49688786eb1c42b2d907b2a9af2d2a6cd
Amy Le <le46t@mtholyoke.edu>
2024-04-11 17:29:41 -0400

    Create ScopeOfVariables.java

---
+++ JavaIntro/src/ScopeOfVariables.java
@@ -0,0 +1,13 @@
+public class ScopeOfVariables {
+    /*
+    * Instance variables are variables that lies in the class but not in a method.
+    * In python, the equivalent of this is a global variable that can be used by every method.
+    * To declare a variable in Java you need to follow the format [TYPE] varName = new [TYPE];
+     */
+    //array of integers
+
+    /*
+    * Java allows you to have a variable without having any value in it, we can declare this by
+    * following the format [TYPE] varName;
+    */
+}
